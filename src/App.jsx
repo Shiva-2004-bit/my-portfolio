ae36013c88b7e82013568253030bec63c5339212
unknown <ahirwarshivnarayan91@gmail.com>
2025-06-16 02:01:28 +0530

    temp: rename Components to temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom'
-import Navbar from './Components/Navbar';
-import Header from './Components/Header';
-import About from './Components/About';
-import Projects from './Components/Projects';
-import Skills from './Components/Skills';
-import Contact from './Components/Contact';
-import Footer from './Components/Footer';
-import NotFound from './Components/NotFound';
+import Navbar from './temp/Navbar';
+import Header from './temp/Header';
+import About from './temp/About';
+import Projects from './temp/Projects';
+import Skills from './temp/Skills';
+import Contact from './temp/Contact';
+import Footer from './temp/Footer';
+import NotFound from './temp/NotFound';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
